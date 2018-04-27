35cd3b52a753b11681d26e52b5b23168a3f5508d
Kayvon Tehranian <ktehranian@gmail.com>
2018-04-27 11:08:00 -0700

    feat: make web3 errors comprehensive and mutually exclusive

--- src/common/web3Errors.ts
+++ src/common/web3Errors.ts
@@ -1,14 +1,16 @@
 const singleLineString = require("single-line-string");
 
 export const web3Errors = {
+    UNABLE_TO_FIND_WEB3_PROVIDER: singleLineString`Make sure that you are using a Web3-enabled
+    browser (such as Chrome with MetaMask installed).`,
+
     UNABLE_TO_FIND_ACCOUNTS: singleLineString`
-        Unable to find active account on current Ethereum network.  Make sure you are using
-        a Web3-enabled browser (such as Chrome with MetaMask installed), that you are connecting
-        to the Kovan testnet, and that your account is unlocked.
+        Unable to find active account on the current Ethereum network
     `,
     UNABLE_TO_FIND_CONTRACTS: singleLineString`
-        Unable to find the Dharma smart contracts on the current Ethereum network.  Make sure you are using
-        a Web3-enabled browser (such as Chrome with MetaMask installed), that you are connecting
-        to the Kovan testnet, and that your account is unlocked.
+        Unable to find the Dharma smart contracts on the current Ethereum network.
     `,
+
+    UNABLE_TO_CONNECT_TO_NETWORK: singleLineString`Please ensure that you are connecting
+        to the appropriate Ethereum network and that your account is unlocked`,
 };
