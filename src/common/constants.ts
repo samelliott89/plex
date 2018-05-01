a2863008941fcc75f10cb1fe345003df207e59ab
chrismin <chrismin@users.noreply.github.com>
2018-05-01 16:06:00 -0700

    build: add mainnet network id to supported network ids (#102)

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,7 +1,12 @@
 export const KOVAN_NETWORK_ID = 42;
 export const LOCAL_NETWORK_ID = 1457;
+export const MAINNET_NETWORK_ID = 1;
 
-export const SUPPORTED_NETWORK_IDS = new Array(KOVAN_NETWORK_ID, LOCAL_NETWORK_ID);
+export const SUPPORTED_NETWORK_IDS = new Array(
+    KOVAN_NETWORK_ID,
+    LOCAL_NETWORK_ID,
+    MAINNET_NETWORK_ID,
+);
 
 export const BLOCKCHAIN_API = {
     POLLING_INTERVAL: 1000,
