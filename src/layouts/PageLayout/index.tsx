0c873c4a8d3ce920738ce8f11268c7c9ca0ff0b8
Graeme <graemelearn@protonmail.com>
2018-05-01 16:58:48 -0700

    fix: better page layout

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -1,27 +1,28 @@
 import * as React from "react";
-import TopNavBar from "../TopNavBar";
+// import TopNavBar from "../TopNavBar";
 import LeftNavBar from "../LeftNavBar";
 import {
-    Wrapper,
-    StyledRow,
-    LeftContainer,
-    RightContainer,
+    // Wrapper,
+    Container,
+    Drawer,
+    Main,
     Footer,
     FooterLink,
+    Layout,
 } from "./styledComponents";
 
 class PageLayout extends React.Component {
     render() {
-        const topLinkItems = [
-            { url: "/plex", display: "PLEX" },
-            { url: "/whitepaper", display: "WHITEPAPER" },
-            { url: "/blog", display: "BLOG" },
-            { url: "/github", display: "GITHUB" },
-            { url: "/chat", display: "CHAT" },
-            { url: "/twitter", display: "TWITTER" },
-            { url: "/terms", display: "TERMS OF USE" },
-            { url: "/privacy", display: "PRIVACY POLICY" },
-        ];
+        // const topLinkItems = [
+        //     { url: "/plex", display: "PLEX" },
+        //     { url: "/whitepaper", display: "WHITEPAPER" },
+        //     { url: "/blog", display: "BLOG" },
+        //     { url: "/github", display: "GITHUB" },
+        //     { url: "/chat", display: "CHAT" },
+        //     { url: "/twitter", display: "TWITTER" },
+        //     { url: "/terms", display: "TERMS OF USE" },
+        //     { url: "/privacy", display: "PRIVACY POLICY" },
+        // ];
 
         const leftLinkItems = [
             { url: "/dashboard", display: "DASHBOARD" },
@@ -30,21 +31,20 @@ class PageLayout extends React.Component {
         ];
 
         return (
-            <Wrapper>
-                <TopNavBar linkItems={topLinkItems} />
-                <StyledRow>
-                    <RightContainer>
+            <Container>
+                <Layout>
+                    <Drawer>
+                        <LeftNavBar linkItems={leftLinkItems} />
+                    </Drawer>
+                    <Main>
                         {this.props.children}
                         <Footer>
                             <FooterLink to="/terms">Terms of Use</FooterLink>
                             <FooterLink to="/privacy">Privacy Policy</FooterLink>
                         </Footer>
-                    </RightContainer>
-                    <LeftContainer>
-                        <LeftNavBar linkItems={leftLinkItems} />
-                    </LeftContainer>
-                </StyledRow>
-            </Wrapper>
+                    </Main>
+                </Layout>
+            </Container>
         );
     }
 }
