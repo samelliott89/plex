bc03211fd95399f0a05a2f8f68fea16b3fe36fe4
Graeme <graemelearn@protonmail.com>
2018-05-02 14:06:11 -0700

    test: remove vestigial tests

--- __test__/unit/layouts/LeftNavBar/LeftNavBar.test.tsx
+++ __test__/unit/layouts/LeftNavBar/LeftNavBar.test.tsx
@@ -6,8 +6,6 @@ import {
     LogoContainer,
     BrandLogo,
     StyledLink,
-    TitleFirst,
-    TitleRest,
 } from "../../../../src/layouts/LeftNavBar/styledComponents";
 import { TradingPermissionsContainer } from "../../../../src/components";
 
@@ -47,56 +45,9 @@ describe("<LeftNavBar />", () => {
                 .find(BrandLogo).length,
         ).toEqual(1);
     });
-
-    it("should render a <StyledLink />", () => {
-        expect(wrapper.find(StyledLink).length).toEqual(1);
-    });
-
+    
     it("should render a <TradingPermissionsContainer /> with class .left", () => {
         expect(wrapper.find(TradingPermissionsContainer).length).toEqual(1);
         expect(wrapper.find(TradingPermissionsContainer).hasClass("left")).toEqual(true);
-    });
-});
-
-describe("<StyledLink />", () => {
-    let wrapper;
-
-    it("should render the correct title", () => {
-        let props = {
-            linkItems: [{ url: "/someurl", display: "Some Url" }],
-        };
-        wrapper = shallow(<LeftNavBar {...props} />);
-        expect(
-            wrapper
-                .find(StyledLink)
-                .first()
-                .find(TitleFirst)
-                .get(0).props.children,
-        ).toEqual("Some");
-        expect(
-            wrapper
-                .find(StyledLink)
-                .first()
-                .find(TitleRest)
-                .get(0).props.children,
-        ).toEqual(" Url");
-        props = {
-            linkItems: [{ url: "/someurl", display: "Title" }],
-        };
-        wrapper = shallow(<LeftNavBar {...props} />);
-        expect(
-            wrapper
-                .find(StyledLink)
-                .first()
-                .find(TitleFirst)
-                .get(0).props.children,
-        ).toEqual("Title");
-        expect(
-            wrapper
-                .find(StyledLink)
-                .first()
-                .find(TitleRest)
-                .get(0).props.children,
-        ).toEqual("");
     });
 });
