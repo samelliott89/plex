bccefaab96b0c86405d291ce0a042745e0aa9625
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-11 16:55:46 -0700

    test: be more explicit in testing token search component

--- __test__/unit/components/TradingPermissions/TokenSearch.test.tsx
+++ __test__/unit/components/TradingPermissions/TokenSearch.test.tsx
@@ -1,6 +1,8 @@
+import * as Web3 from "web3";
 import * as React from "react";
 import { BigNumber } from "bignumber.js";
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
+import Dharma from "@dharmaprotocol/dharma.js";
 
 import MockWeb3 from "../../../../__mocks__/web3";
 import MockDharma from "../../../../__mocks__/dharma.js";
@@ -12,34 +14,46 @@ import {
 } from "../../../../src/components/TradingPermissions/styledComponents";
 
 describe("TokenSearch (Unit)", () => {
-    let web3;
-    let dharma;
-    let props;
+    const mockWeb3 = new MockWeb3() as Web3;
+    const mockDharma = new MockDharma() as Dharma;
 
-    beforeEach(() => {
-        web3 = new MockWeb3();
-        dharma = new MockDharma();
-        props = {
-            tokens: [],
-            web3,
-            dharma,
-            setError: jest.fn(),
-            clearToast: jest.fn(),
-            handleFaucetRequest: jest.fn(),
-            agreeToTerms: true,
-            updateProxyAllowanceAsync: jest.fn(),
-        };
-    });
+    const DEFAULT_PROPS: Props = {
+        agreeToTerms: true,
+        networkId: 1,
+        clearToast: jest.fn(),
+        dharma: mockDharma,
+        handleFaucetRequest: jest.fn(),
+        setError: jest.fn(),
+        tokens: [],
+        updateProxyAllowanceAsync: jest.fn(),
+        web3: mockWeb3,
+    };
+
+    function generateComponent(props: Props = DEFAULT_PROPS): ShallowWrapper {
+        return shallow(
+            <TokenSearch
+                agreeToTerms={props.agreeToTerms}
+                networkId={props.networkId}
+                clearToast={props.clearToast}
+                dharma={props.dharma}
+                handleFaucetRequest={props.handleFaucetRequest}
+                setError={props.setError}
+                tokens={props.tokens}
+                updateProxyAllowanceAsync={props.updateProxyAllowanceAsync}
+                web3={props.web3}
+            />,
+        );
+    }
 
     describe("#render", () => {
-        const tokenSearch = shallow(<TokenSearch {...props} />);
+        const tokenSearchWrapper = generateComponent();
 
         it("should render", () => {
-            expect(tokenSearch.length).toEqual(1);
+            expect(tokenSearchWrapper.length).toEqual(1);
         });
 
         it("should render <NoTokenResults /> if there are no tokens", () => {
-            expect(tokenSearch.find(NoTokenResults).length).toEqual(1);
+            expect(tokenSearchWrapper.find(NoTokenResults).length).toEqual(1);
         });
     });
 });
